2f0b9caaa1ae498f4651f78562f03af80431bee1
Peter Xu <px.peter.xu@gmail.com>
2013-05-04 03:32:03 -0400

    Added INSERT statement generator for test scripts

---
+++ export_for_test.py
@@ -0,0 +1,23 @@
+import pymysql
+import settings
+import sys
+
+_, db, table = sys.argv
+
+# Export a table for testing using test_flexidata.py
+conn = pymysql.connect(
+    db=db,
+    user=settings.flexidata_username,
+    passwd=settings.flexidata_password,
+    host=settings.flexidata_host)
+
+
+cursor = conn.cursor(pymysql.cursors.DictCursor)
+cursor.execute('SELECT * FROM {table}'.format(table=table))
+results = cursor.fetchall()
+
+for result in results:
+    query = 'INSERT INTO {table} ({columns}) VALUES ({values})'.format(table=table, columns=result.keys(), values=result.items())
+    print query
+
+
